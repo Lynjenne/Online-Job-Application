ee5784c02bea7ea7822ffa8c14dc2a693c807837
Jennelyn Urot Peromingan <perominganj@gmail.com>
2018-05-06 14:05:04 +0800

    Code rebase

--- index.php
+++ index.php
@@ -11,12 +11,6 @@
     <script src="js/jquery-1.12.0.min.js"></script>
     <script src="js/search.js"></script>
     <script src="js/bootstrap.min.js"></script>
-    <!-- jQuery 3 -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <!-- Bootstrap 3.3.7 -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <!-- AdminLTE App -->
-    <script src="js/adminlte.min.js"></script>
     <link href="css/home.css" rel="stylesheet">
     <link rel="stylesheet" href="bootstrap/dist/css/bootstrap.min.css">
     </head>
@@ -58,57 +52,6 @@
             <button type="button" onclick="search()" class="btn btn-lg " style="color: black"><span class="glyphicon glyphicon-search"></span> Search</button>
         </form>
     </div>
-
-<div class="content-wrapper" style="margin-left: 0px;">
-  <section class="content-header">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12 latest-job margin-bottom-20">
-            <h1 class="text-center">Latest Jobs</h1>            
-            <?php 
-          /* Show any 4 random job post
-           * 
-           * Store sql query result in $result variable and loop through it if we have any rows
-           * returned from database. $result->num_rows will return total number of rows returned from database.
-          */
-          $sql = "SELECT * FROM job_post Order By Rand() Limit 4";
-          $result = $conn->query($sql);
-          if($result->num_rows > 0) {
-            while($row = $result->fetch_assoc()) 
-            {
-              $sql1 = "SELECT * FROM company WHERE id_company='$row[id_company]'";
-              $result1 = $conn->query($sql1);
-              if($result1->num_rows > 0) {
-                while($row1 = $result1->fetch_assoc()) 
-                {
-             ?>
-            <div class="attachment-block clearfix">
-              <img class="attachment-img" src="img/photo1.png" alt="Attachment Image">
-              <div class="attachment-pushed">
-                <h4 class="attachment-heading"><a href="view-job-post.php?id=<?php echo $row['id_jobpost']; ?>"><?php echo $row['jobtitle']; ?></a> <span class="attachment-heading pull-right">$<?php echo $row['maximumsalary']; ?>/Month</span></h4>
-                <div class="attachment-text">
-                    <div><strong><?php echo $row1['companyname']; ?> | <?php echo $row1['city']; ?> | Experience <?php echo $row['experience']; ?> Years</strong></div>
-                </div>
-              </div>
-            </div>
-          <?php
-              }
-            }
-            }
-          }
-          ?>
-
-          </div>
-        </div>
-      </div>
-    </section>
- 
 </div> <!-- jumbotron -->
- <footer class="main-footer" style="margin-left: 0px;">
-    <div class="text-center">
-      <strong>Copyright &copy; 2018.</strong> All rights reserved.
-    </div>
-  </footer>
-  <div class="control-sidebar-bg"></div>
 </body>
 </html>
