07511dfea6bd40adaa2ab9ea17a82f4a41646af7
Jennelyn Urot Peromingan <perominganj@gmail.com>
2018-05-27 22:40:51 +0800

    Add connection for development

--- pdo.php
+++ pdo.php
@@ -8,7 +8,10 @@ $db_name   = "online_job";
 
 try {
   //creating new connection object from PDO
-  $conn = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+  //Development 
+  $conn = new PDO("mysql:host=localhost;dbname=jobportal", "root", "");
+  // Production
+  //$conn = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $message) {
